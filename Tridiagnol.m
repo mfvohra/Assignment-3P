ddd3435a67bbf01c933c296599f72c2356bd5363
mfvohra <36613280+mfvohra@users.noreply.github.com>
2018-02-19 05:11:52 -0600

    Add files via upload

---
+++ Tridiagnol.m
@@ -0,0 +1,28 @@
+
+clear all;
+clc;
+
+N = 10;
+k = 10;
+Uo = 1;
+UatL = 0;
+
+A = 1;
+L = 1;
+v = 1;
+h = L/N;
+B = 2+(k^2*h^2);
+a = zeros(N+1);
+f = (h^2)*A*ones(N,1);
+f(1) = f(1) - Uo;
+f(N) = f(N) - UatL;
+
+for j=1:N
+    a(j,j) = -B;
+    a(j,j+1) = 1;
+    a(j+1,j) = 1;
+    
+end
+
+a_trimmed = a(1:N,1:N);
+U = a_trimmed\f
